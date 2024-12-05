e136127f51802fb4c9e52184114a86ab4d37428d
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2024-12-05 11:19:01 +0300

    change lng of back text to so and ar lng

--- src/reusible/BackIcon.jsx
+++ src/reusible/BackIcon.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Link } from 'react-router-dom';
 
+import useLanguage from './useLanguage';
+
 export default function BackIcon({ pathUrl }) {
+
+  const { t } = useLanguage();
+
   return (
     <div>
       <Link to={pathUrl}>
         <div className='mb-4 md:mb-0'>
           <Button aria-label="back" variant='contained' sx={{ backgroundColor: '#3A57E8 ' }}>
             <ArrowBackIcon style={{ color: "white" }} />
-            Back
+          <span className='mr-1 text-lg'>  {t('back')}</span>
           </Button>
         </div>
       </Link>
